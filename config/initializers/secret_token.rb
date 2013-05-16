e31d201bc82a0404c9356522307cc25a2271a128
Lee Miller <iarkeus@gmail.com>
2013-05-16 01:53:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trisphere::Application.config.secret_key_base = 'e506ed0d654bb79af019033b1e3fc4575c56800cfd91bc6523dc285ab1b615837d07025be6bc3d95064231c789073943ec45214cefc5b18b636ee84135364261'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Trisphere::Application.config.session_store :cookie_store, key: '_trisphere_session'
