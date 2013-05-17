07ec91786b7388d174cb66ab982a28d623904724
Lee Miller <iarkeus@gmail.com>
2013-05-17 01:09:26 -0700

    Index controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Trisphere::Application.routes.draw do
+  root "index#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
