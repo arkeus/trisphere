10b1a5370b0cdac21b0f99fad477fba8f708bdd6
Lee Miller <iarkeus@gmail.com>
2013-05-17 01:12:06 -0700

    Testing credential manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Trisphere::Application.routes.draw do
   root "index#index"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
