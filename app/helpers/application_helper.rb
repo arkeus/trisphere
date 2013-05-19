7c9a1a789e90a54e2299bf1e639d319ae5ddd5ee
Lee Miller <iarkeus@gmail.com>
2013-05-19 04:22:30 -0700

    Options button to nav.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-	def navigation_tag(url, icon, text)
+	def navigation_tag(url, icon, text, options = {})
 		icon_url = image_url("layout/nav_icon_#{icon}.png")
-		link_to(text, url, class: "nav-tag", style: "background-image: url('#{icon_url}');")
+		link_to(text, url, class: ["nav-tag"] | Array(options[:class]), style: "background-image: url('#{icon_url}');")
 	end
 end
