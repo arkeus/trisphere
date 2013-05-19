65728850632cc098b55023fbbcb38dbe6036ba0b
Lee Miller <iarkeus@gmail.com>
2013-05-19 02:18:56 -0700

    Route to login.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Trisphere::Application.routes.draw do
   root "index#index"
   
+  post "login" => "index#login"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
