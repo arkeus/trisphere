3d2c4aee523e9d737c00b3a0f25153c2c8564665
Lee Miller <iarkeus@gmail.com>
2013-05-19 02:18:29 -0700

    Login code.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,2 +1,14 @@
 class IndexController < ApplicationController
+	def index; end
+	
+	def login
+		user = User.find_by_username(params[:username]).try(:authenticate, params[:password])
+		if user
+			session[:user_id] = user.id
+			redirect_to controller: :character, action: :home and return
+		else
+			flash[:error] = "Invalid username or password"
+			redirect_to action: :index and return
+		end
+	end
 end
