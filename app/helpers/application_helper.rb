94ece9879d76fc927f66c224a953c5e0313c28cb
Lee Miller <iarkeus@gmail.com>
2013-05-19 19:38:41 -0700

    Character bar helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
 		icon_url = image_url("layout/nav_icon_#{icon}.png")
 		link_to(text, url, class: ["nav-tag"] | Array(options[:class]), style: "background-image: url('#{icon_url}');")
 	end
+	
+	def character_bar(label, image, color, value, min, max, show_progress = false, short = false)
+		render :partial => "shared/character_bar", locals: { label: label, image: image, color: color, value: value, min: min, max: max, show_progress: show_progress, short: short }
+	end
 end
