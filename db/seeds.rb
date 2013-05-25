8c90b0bf63a3700909a858cd9a907d18d279d92b
Lee Miller <iarkeus@gmail.com>
2013-05-25 00:08:08 -0700

    Change account to test account.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-user = User.new(username: "arkeus", password: "password", password_confirmation: "password", email: "iarkeus@gmail.com", ip: "134.134.134.134")
+user = User.new(username: "tester01", password: "test01", password_confirmation: "test01", email: "iarkeus@gmail.com", ip: "134.134.134.134", signature: "This is a motherfucking signature", avatar: "http://s13.postimage.org/fnei7jztv/autumn.png")
 user.save!
 
 character = Character.new(user_id: 1, active: true, name: "Arkeus")
+character.save!
+
+character = Character.new(user_id: 1, active: false, name: "Kal")
 character.save!
 
 ForumCategory.new(name: "General Chat", description: "Chat about anything to do with Trisphere.").save!
