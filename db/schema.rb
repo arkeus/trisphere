9e8dcb0afa606eb8578246df3d307ea9fd9179ef
Lee Miller <iarkeus@gmail.com>
2013-09-09 01:30:11 -0700

    Updated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 20130604034709) do
   create_table "friendships", force: true do |t|
     t.integer  "user_id",    null: false
     t.integer  "friend_id",  null: false
+    t.string   "note"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
