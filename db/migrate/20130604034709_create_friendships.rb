73a22cf0c78773f365cf7e3e215d9ef472cb3e3a
Lee Miller <iarkeus@gmail.com>
2013-09-09 01:29:53 -0700

    Add note to friendships.

--- db/migrate/20130604034709_create_friendships.rb
+++ db/migrate/20130604034709_create_friendships.rb
@@ -3,6 +3,7 @@ class CreateFriendships < ActiveRecord::Migration
     create_table :friendships do |t|
 			t.integer :user_id, null: false
 			t.integer :friend_id, null: false
+			t.string :note, limit: 255
       t.timestamps
     end
   end
