77cac19d5cd7c7411b436cbcf2baa176de13fcb7
Lee Miller <iarkeus@gmail.com>
2013-09-10 00:24:29 -0700

    Creating users map used for profile linkes.

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,6 @@
 class FriendsController < ApplicationController
 	def index
 		@friendships = @user.friendships.includes(:friend)
+		@users = Hash[*@friendships.map { |friend| [friend.friend.id, friend.friend] }.flatten]
 	end
 end
