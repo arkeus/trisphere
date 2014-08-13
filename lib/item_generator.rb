0acdef1222ea508926c41557920c5ebbcba6a487
Lee Miller <iarkeus@gmail.com>
2014-08-13 00:39:58 -0700

    Skip returning nil items.

--- lib/item_generator.rb
+++ lib/item_generator.rb
@@ -5,6 +5,7 @@ require "item_lib"
 class ItemGenerator
 	def self.equipment(min_level, max_level, type = nil, subtype = nil, prefix_chance = 0.5, suffix_chance = 0.5)
 		base = BaseItem.database.select { |i| i.level >= min_level && i.level <= max_level && (type.nil? || i.type == type) && (subtype.nil? || i.subtype == subtype) }.sample
+		return nil unless base
 		item = Item.new
 		item.base = base
 		item.prefix = Affix.random(Affix::PREFIX) if rand <= prefix_chance
