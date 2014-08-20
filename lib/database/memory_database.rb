cd3f2f3e42ef1d2b01b838c1ed33b2405a4902b7
Lee Miller <iarkeus@gmail.com>
2014-08-19 21:59:05 -0700

    Figuring out how to handle in-memory databasii.

---
+++ lib/database/memory_database.rb
@@ -0,0 +1,28 @@
+class MemoryDatabase
+	def initialize
+		@data = validate generate_data
+		@id_map = @data.inject({}) { |acc, entry| acc[entry.id] = entry; acc }
+		@name_map = @data.inject({}) { |acc, entry| acc[entry.name] = entry; acc }
+	end
+	
+	def find(target)
+		entry = target.is_a?(Integer) ? @id_map[target] : @name_map[target]
+		raise "Cannot find memory database entry via '#{target}'" unless entry
+		entry
+	end
+	
+	protected
+	
+	def generate_data
+		raise "Memory databases must implement generate_data"
+	end
+	
+	private
+	
+	def validate(data)
+		data.each do |entry|
+			raise "MemoryDatabase entries must respond to id" unless entry.respond_to? :id
+			raise "MemoryDatabase entries must respond to name" unless entry.respond_to? :name
+		end
+	end
+end
