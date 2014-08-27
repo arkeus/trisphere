784feac41d17456b4457a2843d9ba5889e168c51
Lee Miller <iarkeus@gmail.com>
2014-08-27 01:44:43 -0700

    Figuring out how to build the skill database.

--- lib/database/skill_database.rb
+++ lib/database/skill_database.rb
@@ -6,4 +6,12 @@ class SkillDatabase < MemoryDatabase
 	end
 	
 	private
+	
+	KNIGHT_ID = 10_00_00
+	ROGUE_ID  = 11_00_00
+	WIZARD_ID = 12_00_00
+	CLERIC_ID = 13_00_00
+	ARCHER_ID = 14_00_00
+	
+	TIER_ID = 10_00
 end
